d04003c0854051f28d0aa2d16d2b098bf4c1df19
Loc Do <thuyloc988@gmail.com>
2022-12-04 19:58:18 +0700

    Add init method.

--- sui_wallet_auto_gen_acc/MainWindow.xaml.cs
+++ sui_wallet_auto_gen_acc/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +23,29 @@ namespace sui_wallet_auto_gen_acc
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string SUI_WALLET_EXTENSION_PATH = "D:/SideProjects/SuiWallet/sui_wallet_auto_gen_acc/opcgpfmipidbgpenhmajoajpbobppdil.crx";
+        private string GET_START_BUTTON_CLASS = "//a[@class='uDUBihzsBafACO5RMGr7 btn primary']";
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnSwitch_Click(object sender, RoutedEventArgs e)
+        {
+            GenerateAccount();
+        }
+
+        private void GenerateAccount()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddExtension(SUI_WALLET_EXTENSION_PATH);
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+            service.SuppressInitialDiagnosticInformation = true;
+            ChromeDriver chromeDriver = new ChromeDriver(service, options);
+            chromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            var getStartBtn = chromeDriver.FindElements(By.XPath(GET_START_BUTTON_CLASS));
         }
     }
 }
