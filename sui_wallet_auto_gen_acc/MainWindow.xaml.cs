0ced24f321319c612817228a9a6837d9be4ec41f
Loc Do <thuyloc988@gmail.com>
2022-12-04 22:28:32 +0700

    Add material design and init method.

--- sui_wallet_auto_gen_acc/MainWindow.xaml.cs
+++ sui_wallet_auto_gen_acc/MainWindow.xaml.cs
@@ -23,8 +23,13 @@ namespace sui_wallet_auto_gen_acc
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string SUI_WALLET_EXTENSION_PATH = "D:/SideProjects/SuiWallet/sui_wallet_auto_gen_acc/opcgpfmipidbgpenhmajoajpbobppdil.crx";
-        private string GET_START_BUTTON_CLASS = "//a[@class='uDUBihzsBafACO5RMGr7 btn primary']";
+        private string chromeExtentionPath = "D:/SideProjects/SuiWallet/sui_wallet_auto_gen_acc/opcgpfmipidbgpenhmajoajpbobppdil.crx";
+        private string fileAccountPath = string.Empty;
+        private bool isChromePathSelected;
+        private bool isAccountPathSelected;
+        private string accountPassword = "kiepdoden@123";
+        private string SUI_WALLET_CREATE_URL = "chrome-extension://opcgpfmipidbgpenhmajoajpbobppdil/ui.html#/initialize/create";
+
 
         public MainWindow()
         {
@@ -38,14 +43,51 @@ namespace sui_wallet_auto_gen_acc
 
         private void GenerateAccount()
         {
+            //if (!isAccountPathSelected && !isChromePathSelected)
+            //{
+            //    MessageBox.Show("Please select all the paths and try again.", "Start error", MessageBoxButton.OK, MessageBoxImage.Error);
+            //    return;
+            //}
             ChromeOptions options = new ChromeOptions();
-            options.AddExtension(SUI_WALLET_EXTENSION_PATH);
+            options.AddExtension(chromeExtentionPath);
+
             ChromeDriverService service = ChromeDriverService.CreateDefaultService();
             service.HideCommandPromptWindow = true;
             service.SuppressInitialDiagnosticInformation = true;
+
             ChromeDriver chromeDriver = new ChromeDriver(service, options);
-            chromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-            var getStartBtn = chromeDriver.FindElements(By.XPath(GET_START_BUTTON_CLASS));
+            chromeDriver.Url = SUI_WALLET_CREATE_URL;
+            var tabs = chromeDriver.WindowHandles;
+            if (tabs.Count > 1)
+            {
+                chromeDriver.SwitchTo().Window(tabs[1]);
+                chromeDriver.Close();
+                chromeDriver.SwitchTo().Window(tabs[0]);
+            }
+            var passwordBoxes = chromeDriver.FindElements(By.ClassName("TH2aGaKvkCfbW1zH9HHX"));
+            var createPasswordBox = passwordBoxes[0];
+            createPasswordBox.SendKeys(accountPassword);
+            var confirmPasswordBox = passwordBoxes[1];
+            confirmPasswordBox.SendKeys(accountPassword);
+            var checkboxArgee = chromeDriver.FindElement(By.ClassName("MmDbA0GzFptVTURsV5YU"));
+            checkboxArgee.Click();
+            var buttonCreate = chromeDriver.FindElement(By.XPath("//button[@class='qvmPyb8NdzsMrFWoHAEN SH1FDQZ9VX53OguPvdQU Ha6uGJWJ9hrjJCBvvs5T']"));
+            buttonCreate.Click();
+        }
+
+        private void buttonChromePath_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonAccountPath_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Keyboard.ClearFocus();
         }
     }
 }
