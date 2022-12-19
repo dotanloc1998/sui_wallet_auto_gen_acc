139b675e171b08b284c3b6d46c442f84006e0e24
Loc Do <thuyloc988@gmail.com>
2022-12-19 20:17:00 +0700

    Add auto twitter.

--- sui_wallet_auto_gen_acc/MainWindow.xaml.cs
+++ sui_wallet_auto_gen_acc/MainWindow.xaml.cs
@@ -6,6 +6,9 @@ using System.IO;
 using System.Windows;
 using System.Windows.Forms;
 using System.Windows.Input;
+using System.Windows.Shapes;
+using static System.Windows.Forms.Design.AxImporter;
+using static System.Windows.Forms.LinkLabel;
 using MessageBox = System.Windows.MessageBox;
 using OpenFileDialog = System.Windows.Forms.OpenFileDialog;
 
@@ -33,14 +36,29 @@ namespace sui_wallet_auto_gen_acc
         private const string DEFAULT_ACCOUNT_PASSWORD = "kiepdoden@123";
         private const string ACCOUNT_FILE_NAME = "\\accounts.txt";
         private const double WAIT_FOR_ELEMENT = 5;
+        private const string TWITTER_LOGIN_PATH = "https://twitter.com";
+        private const string TWITTER_INPUT_XPATH = "//input[@class='r-30o5oe r-1niwhzg r-17gur6a r-1yadl64 r-deolkf r-homxoj r-poiln3 r-7cikom r-1ny4l3l r-t60dpp r-1dz5y72 r-fdjqy7 r-13qz1uu']";
+        private const string TWITTER_NEXT_BTN_XPATH = "//div[@class='css-18t94o4 css-1dbjc4n r-42olwf r-sdzlij r-1phboty r-rs99b7 r-ywje51 r-usiww2 r-2yi16 r-1qi8awa r-1ny4l3l r-ymttw5 r-o7ynqc r-6416eg r-lrvibr r-13qz1uu']";
+        private const string TWITTER_LOGIN_BTN_XPATH = "//div[@data-testid='LoginForm_Login_Button']";
+        private const string TWITTER_TWEET_BOX_XPATH = "//div[@class='public-DraftStyleDefault-block public-DraftStyleDefault-ltr']";
+        private const string TWITTER_TWEET_BTN_XPATH = "//div[@class='css-18t94o4 css-1dbjc4n r-l5o3uw r-42olwf r-sdzlij r-1phboty r-rs99b7 r-19u6a5r r-2yi16 r-1qi8awa r-1ny4l3l r-ymttw5 r-o7ynqc r-6416eg r-lrvibr']";
+        private const string TWITTER_SWITCH_ACC_BTN_XPATH = "//div[@data-testid='SideNav_AccountSwitcher_Button']";
+        private const string TWITTER_LOGOUT_ACC_BTN_XPATH = "//a[@data-testid='AccountSwitcher_Logout_Button']";
+        private const string TWITTER_LOGOUT_CONFIRM_BTN_XPATH = "//div[@data-testid='confirmationSheetConfirm']";
+        private const string TWITTER_LOGIN_MAIN_BTN_XPATH = "//a[@data-testid='login']";
+        private const string TWITTER_LOGIN_MAIN_BTN2_XPATH = "//a[@data-testid='loginButton']";
         #endregion
 
         private string chromeExtentionPath = string.Empty;
+        private string fileAccountTwitterPath = string.Empty;
         private string fileAccountPath = string.Empty;
         private bool isChromePathSelected;
         private bool isAccountPathSelected;
+        private bool isAccountTwitterPathSelected;
         private string accountPassword = string.Empty;
+        private string twitterStatus = string.Empty;
         private StreamWriter sw;
+        private StreamReader sr;
         private FolderBrowserDialog folderBrowserDialog;
         private OpenFileDialog openFileDialog;
 
@@ -66,14 +84,7 @@ namespace sui_wallet_auto_gen_acc
             ChromeOptions options = new ChromeOptions();
             options.AddExtension(chromeExtentionPath);
 
-            //Hide console prompt
-            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
-            service.HideCommandPromptWindow = true;
-            service.SuppressInitialDiagnosticInformation = true;
-
-            //Create Chrome driver
-            ChromeDriver chromeDriver = new ChromeDriver(service, options);
-            chromeDriver.Url = SUI_WALLET_CREATE_URL;
+            ChromeDriver chromeDriver = CreateChromeDriver(options, SUI_WALLET_CREATE_URL);
             var tabs = chromeDriver.WindowHandles;
             if (tabs.Count > 1)
             {
@@ -81,7 +92,6 @@ namespace sui_wallet_auto_gen_acc
                 chromeDriver.Close();
                 chromeDriver.SwitchTo().Window(tabs[0]);
             }
-            chromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(WAIT_FOR_ELEMENT);
 
             //Start creating account
             accountPassword = !string.IsNullOrWhiteSpace(textboxPassword.Text.Trim()) ? textboxPassword.Text.Trim() : DEFAULT_ACCOUNT_PASSWORD;
@@ -159,6 +169,28 @@ namespace sui_wallet_auto_gen_acc
             }
         }
 
+        private static ChromeDriver CreateChromeDriver(ChromeOptions? options = null, string url = "")
+        {
+            //Hide console prompt
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+            service.SuppressInitialDiagnosticInformation = true;
+
+            ChromeDriver chromeDriver;
+            if (options != null)
+            {
+                chromeDriver = new ChromeDriver(service, options);
+            }
+            else
+            {
+                chromeDriver = new ChromeDriver(service);
+            }
+            chromeDriver.Url = url;
+            chromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(WAIT_FOR_ELEMENT);
+
+            return chromeDriver;
+        }
+
         private void WriteFile(string content)
         {
             sw = new StreamWriter(fileAccountPath + ACCOUNT_FILE_NAME, append: true);
@@ -203,6 +235,115 @@ namespace sui_wallet_auto_gen_acc
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             Keyboard.ClearFocus();
+        }
+
+        private void btnCommentSwitch_Click(object sender, RoutedEventArgs e)
+        {
+            StartAutoTwitter();
+        }
+
+        private void StartAutoTwitter()
+        {
+            if (!isAccountTwitterPathSelected)
+            {
+                MessageBox.Show("Please select the accounts file path and try again.", "Start error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            sr = new StreamReader(fileAccountTwitterPath);
+            ChromeDriver chromeDriver = CreateChromeDriver(null, TWITTER_LOGIN_PATH);
+            string twitterLine;
+            string[] twitterAccount;
+            Random random = new Random();
+            try
+            {
+                while ((twitterLine = sr.ReadLine()) != null)
+                {
+                    twitterAccount = twitterLine.Split(' ');
+                    if (twitterAccount.Length == 0)
+                    {
+                        break;
+                    }
+                    try
+                    {
+                        var buttonLoginMain = chromeDriver.FindElement(By.XPath(TWITTER_LOGIN_MAIN_BTN_XPATH));
+                        buttonLoginMain.Click();
+                    }
+                    catch (NoSuchElementException)
+                    {
+                        var buttonLoginMain = chromeDriver.FindElement(By.XPath(TWITTER_LOGIN_MAIN_BTN2_XPATH));
+                        buttonLoginMain.Click();
+                    }
+                    var userNameBox = chromeDriver.FindElement(By.XPath(TWITTER_INPUT_XPATH));
+                    if (userNameBox != null)
+                    {
+                        userNameBox.SendKeys(twitterAccount[0].Trim());
+                    }
+                    var buttonNext = chromeDriver.FindElement(By.XPath(TWITTER_NEXT_BTN_XPATH));
+                    if (buttonNext != null)
+                    {
+                        buttonNext.Click();
+                    }
+                    var passwordBox = chromeDriver.FindElement(By.XPath(TWITTER_INPUT_XPATH));
+                    if (passwordBox != null)
+                    {
+                        passwordBox.SendKeys(twitterAccount[1].Trim());
+                    }
+                    var buttonLogin = chromeDriver.FindElement(By.XPath(TWITTER_LOGIN_BTN_XPATH));
+                    if (buttonLogin != null)
+                    {
+                        buttonLogin.Click();
+                    }
+                    var twitterBox = chromeDriver.FindElement(By.XPath(TWITTER_TWEET_BOX_XPATH));
+                    if (twitterBox != null)
+                    {
+                        twitterStatus = !string.IsNullOrWhiteSpace(textboxComment.Text.Trim()) ? textboxComment.Text.Trim() : DEFAULT_ACCOUNT_PASSWORD;
+                        twitterBox.SendKeys(twitterStatus + random.Next());
+                    }
+                    var buttonTweet = chromeDriver.FindElement(By.XPath(TWITTER_TWEET_BTN_XPATH));
+                    if (buttonTweet != null)
+                    {
+                        buttonTweet.Click();
+                    }
+                    var buttonSwitchAcc = chromeDriver.FindElement(By.XPath(TWITTER_SWITCH_ACC_BTN_XPATH));
+                    if (buttonSwitchAcc != null)
+                    {
+                        buttonSwitchAcc.Click();
+                    }
+                    var buttonLogoutAcc = chromeDriver.FindElement(By.XPath(TWITTER_LOGOUT_ACC_BTN_XPATH));
+                    if (buttonLogoutAcc != null)
+                    {
+                        buttonLogoutAcc.Click();
+                    }
+                    var buttonLogoutConfirm = chromeDriver.FindElement(By.XPath(TWITTER_LOGOUT_CONFIRM_BTN_XPATH));
+                    if (buttonLogoutConfirm != null)
+                    {
+                        buttonLogoutConfirm.Click();
+                    }
+                }
+                MessageBox.Show("Auto stopped.", "Notification", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+        }
+
+        private void buttonAccountTwPath_Click(object sender, RoutedEventArgs e)
+        {
+            openFileDialog = new OpenFileDialog();
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(openFileDialog.FileName))
+            {
+                fileAccountTwitterPath = openFileDialog.FileName;
+                labelAccountTwPath.Text = fileAccountTwitterPath;
+                isAccountTwitterPathSelected = true;
+            }
+            else
+            {
+                MessageBox.Show("Please select file contants your twitter accounts.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                isAccountTwitterPathSelected = false;
+            }
         }
     }
 }
